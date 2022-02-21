04489317f88d99bef1da4cf7d386e6d9915014ae
pomelo <845192429@qq.com>
2022-02-21 21:57:04 +0800

    简单的配置 nvim 插件

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,2 +1,8 @@
+-- utf8
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = 'utf-8'
+
+-- 显示行号
+vim.wo.number = true
 -- 高亮所在行
 vim.wo.cursorline = true
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,54 @@ return require('packer').startup(function(use)
 
   -------------------------- plugins -------------------------------------------
 
+  -- 代码高亮
+  -- treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- 树结构
+  -- nvim-tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  -- 上方的文件名标签？
+  -- bufferline
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  -------------------------- lsp -------------------------------------------
+
+  -- lspconfig 微软为开发工具提出的一个协议
+  use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+  -- nvim-cmp 代码补全
+  use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+  use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+  use 'hrsh7th/cmp-path'     -- { name = 'path' }
+  use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+  use 'hrsh7th/nvim-cmp'
+  -- vsnip 自定义代码段
+  use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+  use 'hrsh7th/vim-vsnip'
+  use 'rafamadriz/friendly-snippets'
+  -- lspkind 代码提示中，显示分类的小图标支持
+  use 'onsails/lspkind-nvim'
+  -- use 'github/copilot.vim'
+
+  --------------------------- colorscheme ------------------------------------
+  
+  -- onedark
+  use 'ful1e5/onedark.nvim'
+
+  ------------------------------------
+
+
+
+
 end)
 
